8fe9dba0bea2fdbbf8f224b9381aa258b97b8664
nadeto26 <nadezhdakarapetrova@gmail.com>
2023-09-11 23:37:44 +0300

    New

---
+++ 05. Stack of Strings/StartUp.cs
@@ -0,0 +1,31 @@
+﻿ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            StackOfStrings stack = new StackOfStrings();
+
+            Console.WriteLine(stack.IsEmpty());
+
+            stack.AddRange(new List<string>() { "1", "2", "3", "4", "5" });
+
+            Console.WriteLine(stack.IsEmpty());
+
+            foreach(var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+        }
+    }
+}
