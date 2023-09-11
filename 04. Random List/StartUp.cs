9f7e945161a4f83484d01150f90d6d553ab85b06
nadeto26 <nadezhdakarapetrova@gmail.com>
2023-09-11 23:37:09 +0300

    New

---
+++ 04. Random List/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            RandomList list = new RandomList();
+            list.Add("1");
+            list.Add("2");
+            list.Add("3"); 
+            list.Add("4");
+            list.Add("5");
+            list.Add("6");
+
+            Console.WriteLine(list.RandomString());
+
+
+
+        }
+    }
+}
