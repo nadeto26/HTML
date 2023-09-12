5b0e6925de278f50e846823e51004a11c59c8126
nadeto26 <nadezhdakarapetrova@gmail.com>
2023-09-12 23:39:55 +0300

    New

---
+++ 01. Persons/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+
+		public Person (string firstName, string lastName, int age)
+		{
+			FirstName = firstName;
+			LastName = lastName;
+			Age = age;
+		}
+		private string  firstName;
+
+		public string FirstName
+		{
+			get { return firstName; }
+			private set { firstName = value; }
+		}
+
+		private string lastName;
+
+		public string LastName
+		{
+			get { return lastName; }
+		    private	set { lastName = value; }
+		}
+
+		private int age;
+
+		public int Age
+		{
+			get { return age; }
+			private set { age = value; }
+		}
+
+        public override string ToString()
+        {
+			return $"{this.firstName} {this.lastName} is {this.age} years old.";
+        }
+
+
+
+    }
+}
