8d61f0097e93f48c8e27abccd97bfa88996e4a71
nadeto26 <nadezhdakarapetrova@gmail.com>
2023-09-13 17:03:10 +0300

    New

---
+++ 03. Validation/Person.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+
+		public Person (string firstName, string lastName, int age, decimal salary)
+		{
+			FirstName = firstName;
+			LastName = lastName;
+			Age = age;
+			Salary = salary;
+		}
+		private string  firstName;
+
+		public string FirstName
+		{
+	      get 
+			{ 
+				return firstName; 
+			}
+		 private set
+			{
+				if (value.Length<3)
+				{
+					throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+
+                }
+                firstName = value;
+            }
+			
+			   // никой повече няма да може да го променя
+		}
+
+		private string lastName;
+
+		public string LastName
+		{
+			get { return lastName; }
+		    private	set
+			{
+				if (value.Length<3)
+				{
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                lastName = value;
+            }
+			
+		}
+
+		private int age;
+
+		public int Age
+		{
+			get { return age; }
+		    private set
+			{
+				if (value <=0)
+				{
+					throw new ArgumentException("Age cannot be zero or a negative integer!");
+				}
+                age = value;
+            }
+		}
+
+		private decimal salary;
+
+		public decimal Salary
+		{
+			get { return salary; }
+			private set 
+			{
+				if (value<650)
+				{
+                    throw new ArgumentException("Salary cannot be less than 650 leva!");
+                }
+				salary = value;
+			
+			}
+		}
+
+
+		public void IncreaseSalary(decimal percentage)
+		{
+			if (this.Age>30)
+			{
+				this.Salary += salary * percentage / 100;
+			}
+			else
+			{
+                this.Salary += salary * percentage / 200;
+            }
+		}
+
+
+        public override string ToString()
+        {
+			return $"{firstName} {lastName} receives {salary:F2} leva.";
+        }
+
+
+
+    }
+}
---
+++ 04. Team/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Team team = new Team("SoftUni");
+            for (int i=0; i< n;i++)
+            {
+                string[] splitted = Console.ReadLine().Split(" ");
+                int age =int.Parse( splitted[2]);
+                decimal salary = decimal.Parse(splitted[3]);
+
+                Person person = new Person(splitted[0], splitted[1], age, salary);
+
+                team.AddPlayer(person);
+            }
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+
+        }
+    }
+}
