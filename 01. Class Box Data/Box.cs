d6d2aab8e06493b7e6c7e03e4bc53d96cc29f9c5
nadeto26 <nadezhdakarapetrova@gmail.com>
2023-09-14 17:23:33 +0300

    New

---
+++ 01. Class Box Data/Box.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp165
+{
+    public class Box
+    {
+
+        public Box(double lenght, double width, double height)
+        {
+            Length = lenght;
+            Width = width;
+            Height = height;
+        }
+
+
+        private double length;
+
+        public double Length
+        {
+            get { return length; }
+             set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Lenght cannot be zero or negative.");
+                }
+                length = value;
+            }
+        }
+
+        private double width;
+
+        public double Width
+        {
+            get { return width; }
+             set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Width cannot be zero or negative.");
+                }
+                width = value;
+            }
+        }
+
+        private double height;
+
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Height cannot be zero or negative.");
+                }
+                height = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            return 2 * length * width + 2 * length * height + 2 * width * height;
+        }
+
+        public  double LateralSurfaceArea()
+        {
+            return 2* length * height + 2* height * width;
+        }
+
+        public double Volume()
+        {
+            return length * width * height;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ 01. Class Box Data/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp165
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new(length, width, height);
+
+                Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():f2}");
+                Console.WriteLine($"Volume - {box.Volume():f2}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+
+        }
+    }
+}
