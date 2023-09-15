62f79b4496d5555561a62783461db7ae3b38f2ef
nadeto26 <nadezhdakarapetrova@gmail.com>
2023-09-15 17:46:30 +0300

    New

---
+++ 01. Shapes/Rectangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp170
+{
+    public class Rectangle: IDrawable
+    {
+        public Rectangle(int width, int height)
+        {
+            Width = width; 
+            Height = height;
+        }
+
+        public int Width { get;private set; }
+        public int Height { get;private set; }
+
+        public void Draw()
+        {
+            DrawLine(Width, '*', '*');
+            for (int i = 1; i < Height - 1; ++i)
+                DrawLine(Width, '*', ' ');
+            DrawLine(Width, '*', '*');
+        }
+        private void DrawLine(int width, char end, char mid)
+        {
+            Console.Write(end);
+            for (int i = 1; i < width - 1; ++i)
+                Console.Write(mid);
+            Console.WriteLine(end);
+        }
+
+    }
+}
+
---
+++ 01. Shapes/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp170
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var radius = int.Parse(Console.ReadLine());
+            IDrawable circle = new Circle(radius);
+            var width = int.Parse(Console.ReadLine());
+            var height = int.Parse(Console.ReadLine());
+            IDrawable rect = new Rectangle(width, height);
+            circle.Draw();
+            rect.Draw();
+        }
+    }
+}
+
+
