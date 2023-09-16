106738c7a481bf7e5ecb79096a6fe3071d891db3
nadeto26 <nadezhdakarapetrova@gmail.com>
2023-09-16 15:00:57 +0300

    New

---
+++ 01. Math Operations/MathOperations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operations
+{
+    public class MathOperations
+    {
+        public int Add(int x, int y) => x + y;
+
+        public double Add(double x, double y, double z) => x + y + z;
+
+        public decimal Add (decimal x, decimal y, decimal z) => x + y + z;
+        
+    }
+}
